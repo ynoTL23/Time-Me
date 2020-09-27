3931aff24afb7fa461c175a39b5f8254de1114c2
ynoTL <tonylu23@gmail.com>
2020-09-27 17:28:46 -0400

    add form functionality

--- src/components/addTimeEntryForm.js
+++ src/components/addTimeEntryForm.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import firebase from '../firebase';
 
 const AddTimeEntryForm = () => {
+  const [title, setTitle] = useState('');
+  const [time, setTime] = useState('');
+
+  function addTimeEntry(e) {
+    e.preventDefault();
+
+    firebase
+      .firestore()
+      .collection('times')
+      .add({
+        title,
+        seconds: parseInt(time)
+      })
+      .then(() => {
+        setTitle('');
+        setTime('');
+      });
+  }
+
   return (
-    <form>
+    <form onSubmit={addTimeEntry}>
       <h4>Add a Time</h4>
       <div>
         <label>Title</label>
-        <input type='text' />
+        <input
+          type='text'
+          value={title}
+          onChange={e => setTitle(e.currentTarget.value)}
+        />
       </div>
       <div>
         <label>Time</label>
-        <input type='number' />
+        <input
+          type='number'
+          value={time}
+          onChange={e => setTime(e.currentTarget.value)}
+        />
       </div>
       <button>Add Time</button>
     </form>
--- src/components/timesList.js
+++ src/components/timesList.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from '../firebase';
+
+function useTimes() {
+  const [times, setTimes] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = firebase
+      .firestore()
+      .collection('times')
+      .onSnapshot(snapshot => {
+        const newTimes = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+
+        setTimes(newTimes);
+      });
+
+    return () => unsubscribe();
+  }, []);
+
+  return times;
+}
 
 const TimesList = () => {
+  const times = useTimes();
+
   return (
     <div>
       <label>Sort By: </label>
@@ -13,30 +38,14 @@ const TimesList = () => {
       </select>
 
       <ol>
-        <li>
-          <div className="time-entry">
-            Entry Title
-            <code className="time">10 seconds</code>
-          </div>
-        </li>
-        <li>
-          <div className="time-entry">
-            Entry Title
-            <code className="time">10 seconds</code>
-          </div>
-        </li>
-        <li>
-          <div className="time-entry">
-            Entry Title
-            <code className="time">10 seconds</code>
-          </div>
-        </li>
-        <li>
-          <div className="time-entry">
-            Entry Title
-            <code className="time">10 seconds</code>
-          </div>
-        </li>
+        {times.map(time => (
+          <li key={times.id}>
+            <div className='time-entry'>
+              {time.title}
+              <code className='time'>{time.seconds} seconds</code>
+            </div>
+          </li>
+        ))}
       </ol>
     </div>
   );
